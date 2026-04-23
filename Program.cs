67b21355e64644fdfc11e09fe17b2bc946370e4a
giuliaalencar <giulia.alencar@gsw.com.br>
2026-04-23 11:20:31 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,11 +16,11 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.UseCors("PermitirTudo");
 app.UseAuthentication();
